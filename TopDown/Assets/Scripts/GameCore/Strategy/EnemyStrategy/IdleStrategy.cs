658a792dfecd7de297ab6fe928cb0b6c89751553
Noob_ka <azzatguru@gmail.com>
2019-10-26 20:57:36 +0400

    Добавление новой стратегии - Отступления. Изменение стратегии - Охоты, слежение с упреждением. Работоспособность связана с учетом проверки на интеллект - isSmart.

--- TopDown/Assets/Scripts/GameCore/Strategy/EnemyStrategy/IdleStrategy.cs
+++ TopDown/Assets/Scripts/GameCore/Strategy/EnemyStrategy/IdleStrategy.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using Character;
 using GameCore.StateMachine;
 
@@ -8,32 +7,40 @@ namespace GameCore.Strategy
 {
     class IdleStrategy : BasicEnemyStategy
     {
-        private Vector3 randomPoint;
-        private float timer;
-        private float distance;
+        private Vector3 _randomPoint;
+        private float _timerHeal;
+        private float _timer;
+        private float _distance;
         private bool isPatrol;
 
         public IdleStrategy(AbstractCharacter owner, string status) : base(owner, status)
         {
-            timer = Time.time + Random.Range(5f, 15f);
+            _timer = Time.time + Random.Range(5f, 15f);
             isPatrol = false;
         }
 
         public override void DoStrategy()
         {
             base.DoStrategy();
-            if (Time.time >= timer && !isPatrol) StartMoveToPoint();
+            IdleHealing();
+            if (Time.time >= _timer && !isPatrol) StartMoveToPoint();
             CheckDistanceToPoint();
-            if (distance < navigationController.GetAgentStopDistance() && isPatrol) EndMoveToPoint();
-            stateMachine.Work();
+            if (_distance < navigationController.GetAgentStopDistance() && isPatrol) EndMoveToPoint();
         }
-
+        
+        private void IdleHealing()
+        {
+            if (Time.time < _timerHeal && !statusController.CheckCurrentHealthIsMax()) return;
+            _timerHeal = Time.time + 5f;
+            statusController.TakeHeal(enemy.healingPower);
+            statusController.RefreshHelth(ref enemy.hp_test);
+        }
 
         private void StartMoveToPoint()
         {
             var point = Random.insideUnitCircle * Random.Range(15f, 30f);
-            randomPoint = enemy.startPosition + new Vector3(point.x, 0f, point.y);
-            navigationController.SetCurrentPoint(randomPoint);
+            _randomPoint = enemy.startPosition + new Vector3(point.x, 0f, point.y);
+            navigationController.SetCurrentPoint(_randomPoint);
             stateMachine.ChangeState(typeof(Move));
             isPatrol = true;
         }
@@ -41,13 +48,13 @@ namespace GameCore.Strategy
         private void CheckDistanceToPoint()
         {
             if (statusController.isHunting || !isPatrol) return;
-            distance = (enemyTransform.position - randomPoint).magnitude;
+            _distance = (enemyTransform.position - _randomPoint).magnitude;
         }
 
         private void EndMoveToPoint()
         {
             isPatrol = false;
-            timer = Time.time + Random.Range(5f, 15f);
+            _timer = Time.time + Random.Range(5f, 15f);
             stateMachine.ChangeState(typeof(Idle));
         }
     }
---
+++ TopDown/Assets/Scripts/GameCore/Strategy/EnemyStrategy/RetreatStrategy.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using GameCore.StateMachine;
+using Character;
+
+namespace GameCore.Strategy
+{
+    class RetreatStrategy : BasicEnemyStategy
+    {
+        private Vector3 _point;
+        private float _healingTimer;
+        private float _lastTimeRetreat = 0f;
+
+        public RetreatStrategy(AbstractCharacter owner, string status) : base(owner, status)
+        {
+        }
+
+        public override void DoStrategy()
+        {
+            base.DoStrategy();
+            if (!statusController.isRetreat) SetRetreatPoint();
+            else WorkRetreat();
+        }
+
+        private void SetRetreatPoint()
+        {
+            Vector3 direction = (enemy.targetTransform.position + enemyTransform.position).normalized;
+            _point = enemyTransform.position + direction * enemy.visionRadius * 1.5f;
+
+            navigationController.SetCurrentPoint(_point);
+            navigationController.SetAgentSpeed(statusController.maxSpeed * 1.25f);
+
+            statusController.isRetreat = true;
+            statusController.isHunting = false;
+
+            _lastTimeRetreat = Time.time + 120f;
+            stateMachine.ChangeState(typeof(Move));
+        }
+
+        private void WorkRetreat()
+        {
+            RefreshRetreatStatus();
+            QuickHealing();
+            if (!CheckDistanceToPoint()) return;
+            StopMoveToPoint();
+        }
+
+        private void RefreshRetreatStatus()
+        {
+            if (!statusController.ChechCurrentHealthToLimit(Mathf.RoundToInt(statusController.maxHealth * 0.6f))) statusController.isRetreat = false;
+            if (statusController.ChechCurrentHealthToLimit(Mathf.RoundToInt(statusController.maxHealth * 0.8f))) return;
+            _point = Vector3.zero;
+        }
+
+        private bool CheckDistanceToPoint()
+        {
+            bool check = (enemyTransform.position - _point).magnitude < navigationController.GetAgentStopDistance();
+            return check;
+        }
+
+        private void QuickHealing()
+        {
+            if (Time.time >= _healingTimer)
+            {
+                _healingTimer = Time.time + 1.5f;
+                statusController.TakeHeal(enemy.healingPower);
+                statusController.RefreshHelth(ref enemy.hp_test);//test visual
+            }
+        }
+
+        private void StopMoveToPoint()
+        {
+            navigationController.SetAgentSpeed(statusController.maxSpeed * 0.5f);//!!!
+            stateMachine.ChangeState(typeof(Idle));
+        }
+    }
+}
